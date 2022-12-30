64a582a78c41bf308bbbd690588377cf0981c5f5
mbridak <michael.bridak@gmail.com>
2022-12-30 08:35:08 -0800

    exit if already running.

--- serial_monitor/__main__.py
+++ serial_monitor/__main__.py
@@ -2,12 +2,25 @@
 # pylint: disable=wrong-import-position
 from pathlib import Path
 import os
+import sys
+import re
 import time
+import pkgutil
+import psutil
 import gi
 from serial.tools.list_ports import comports
 
 gi.require_version("Notify", "0.7")
 from gi.repository import Notify, GdkPixbuf
+
+
+def check_process() -> int:
+    """checks to see if flrig is in the active process list"""
+    count = 0
+    for proc in psutil.process_iter():
+        if bool(re.match("serial_monitor", proc.name().lower())):
+            count += 1
+    return count
 
 
 class PortChecker:
@@ -43,6 +56,19 @@ class PortChecker:
 
 def main():
     """main entry"""
+    process_count = check_process()
+    if process_count > 1:
+        sys.exit(0)
+    path = os.path.dirname(pkgutil.get_loader("serial_monitor").get_filename())
+    os.system(
+        "xdg-icon-resource install --size 64 --context apps --mode user "
+        f"{path}/k6gte-serial_monitor-64.png k6gte-serial_monitor"
+    )
+    os.system(
+        "xdg-icon-resource install --size 32 --context apps --mode user "
+        f"{path}/k6gte-serial_monitor-32.png k6gte-serial_monitor"
+    )
+    os.system(f"xdg-desktop-menu install {path}/k6gte-serial_monitor.desktop")
     os.chdir(Path(__file__).parent)
     job = PortChecker()
     while True:
